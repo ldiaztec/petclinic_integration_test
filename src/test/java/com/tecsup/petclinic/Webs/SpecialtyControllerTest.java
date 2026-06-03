17f8349ebfc48f74e6d64bfad6e7de35d8c8fbb4
brauliomendoza-debug <braulio.mendoza@tecsup.edu.pe>
2026-06-03 17:00:20 -0500

    Tercer cambio

--- src/test/java/com/tecsup/petclinic/Webs/SpecialtyControllerTest.java
+++ src/test/java/com/tecsup/petclinic/Webs/SpecialtyControllerTest.java
@@ -1,14 +1,19 @@
 package com.tecsup.petclinic.webs;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,37 +22,48 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SpecialtyControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
+    @Order(1)
     public void testFindAllSpecialties() throws Exception {
-        this.mockMvc.perform(get("/api/v1/specialties"))
+        this.mockMvc.perform(get("/api/specialties"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE));
+                .andExpect(jsonPath("$._embedded.specialties", notNullValue()));
     }
 
     @Test
+    @Order(2)
     public void testFindSpecialtyById() throws Exception {
-        this.mockMvc.perform(get("/api/v1/specialties/1"))
+        this.mockMvc.perform(get("/api/specialties/2"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id", is(1)))
-                .andExpect(jsonPath("$.name", is("radiology")));
+                .andExpect(jsonPath("$.name", is("surgery")));
     }
 
     @Test
+    @Order(3)
     public void testCreateSpecialty() throws Exception {
-        String jsonSpecialty = "{\"name\":\"Cardiology\"}";
+        String jsonSpecialty = "{\"name\":\"dermatology\"}";
 
-        this.mockMvc.perform(post("/api/v1/specialties")
+        this.mockMvc.perform(post("/api/specialties")
                 .content(jsonSpecialty)
-                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.name", is("Cardiology")));
+                .andExpect(header().string("Location", notNullValue()));
+    }
+
+    @Test
+    @Order(4)
+    public void testDeleteSpecialty() throws Exception {
+        this.mockMvc.perform(delete("/api/specialties/1"))
+                .andDo(print())
+                .andExpect(status().isNoContent());
     }
 }
