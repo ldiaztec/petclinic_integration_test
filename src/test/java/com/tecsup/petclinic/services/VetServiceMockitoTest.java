35de64aeab560dcd994bdbb363c6c150e802d822
ldiaz <ldiaza@tecsup.edu.pe>
2026-06-03 12:08:08 -0500

    Repo integration

---
+++ src/test/java/com/tecsup/petclinic/services/VetServiceMockitoTest.java
@@ -0,0 +1,11 @@
+package com.tecsup.petclinic.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+public class VetServiceMockitoTest {
+
+    // TO DO
+}
